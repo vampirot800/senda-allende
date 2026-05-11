8d5e247a08d3f7210091625aeb97fb16471fbda2
rfloresv <rflo0001@student.monash.edu>
2026-05-10 18:22:09 -0600

    corrección formato

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -42,7 +42,6 @@ export default function Hero() {
           </a>
         </div>
         <div className="absolute bottom-16 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 text-white/40">
-          <span className="font-sans text-[9px] tracking-widest uppercase">Explorar</span>
           <div className="w-px h-10 bg-gradient-to-b from-white/40 to-transparent animate-pulse" />
         </div>
       </div>
